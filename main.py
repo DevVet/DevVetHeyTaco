0cfa06fa3784773d66f8c39fa1b3dd43faf782d6
Benjamin Winchester <benjamin.m.winchester@googlemail.com>
2021-02-10 10:18:38 -0500

    More formatting

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
-import datetime
-from dotenv import load_dotenv
+from datetime import datetime
+
 import discord
 from discord_slash import SlashCommand
-from pg2db import init_db_conn, has_given_today, add_transaction, get_this_months_scores
+from dotenv import load_dotenv
 
+from pg2db import init_db_conn, has_given_today, add_transaction, get_this_months_scores
 
 MONTH = [
     "January",
@@ -19,7 +20,7 @@ MONTH = [
     "October",
     "November",
     "December"
-    ]
+]
 
 HEY_TACO_BOT_ID = '785953175865524244'
 
@@ -61,60 +62,62 @@ class MyClient(discord.Client):
             data = get_this_months_scores(self.conn)
             now = datetime.now()
             embed = discord.Embed(
-                title=f"Scores for {MONTH[now.month - 1]} {now.year}", 
+                title=f"Scores for {MONTH[now.month - 1]} {now.year}",
                 description=f"A total of {sum([taco[1] for taco in data])} \
                     have been given this month"
             )
             for taco in data:
                 guild_member_name = await message.channel.guild.fetch_member(taco[0])
                 embed.add_field(
-                    name=guild_member_name.display_name, 
-                    value=taco[1], 
+                    name=guild_member_name.display_name,
+                    value=taco[1],
                     inline=False
                 )
 
             await message.channel.send(embed=embed)
 
 
-if __name__ == "__main__":
-    load_dotenv()
-    client = MyClient(intents=discord.Intents.all())
-    slash = SlashCommand(client, auto_register=True)
+client = MyClient(intents=discord.Intents.all())
+slash = SlashCommand(client, auto_register=True)
 
-    DEVVET_GUILD_ID = 765949464443617331
-    guild_ids = [DEVVET_GUILD_ID]
+DEVVET_GUILD_ID = 765949464443617331
+guild_ids = [DEVVET_GUILD_ID]
 
-    @slash.slash(
-        name="tacoscores",
-        description="Returns this month's taco scores",
-        guild_ids=guild_ids
-    )
-    async def _send_tacoscores(ctx):
-        data = get_this_months_scores(init_db_conn())
 
-        now = datetime.now()
+@slash.slash(
+    name="tacoscores",
+    description="Returns this month's taco scores",
+    guild_ids=guild_ids
+)
+async def _send_taco_scores(ctx):
+    data = get_this_months_scores(init_db_conn())
 
-        if len(data) > 0:
-            await ctx.channel.send(
-                f"***Scores for{MONTH[now.month - 1]} {now.year}***.\
-                \nA total of {sum([taco[1] for taco in data])} \
-                have been given this month"
-            )
+    now = datetime.now()
 
-            for taco in data:
-                guild_member = await ctx.channel.guild.fetch_member(taco[0])
-                embed = discord.Embed(title=taco[1])
-                embed.set_author(
-                    name=guild_member.display_name, 
-                    icon_url=guild_member.avatar_url
-                )
-                await ctx.channel.send(embed=embed)
+    if len(data) > 0:
+        await ctx.channel.send(
+            f"***Scores for{MONTH[now.month - 1]} {now.year}***.\nA total of {sum([taco[1] for taco in data])} have "
+            f"been given this month "
+        )
 
-        else:
-            embed = discord.Embed(
-                title="No tacos awarded this month", 
-                description="Go get some Tacos!"
+        for taco in data:
+            guild_member = await ctx.channel.guild.fetch_member(taco[0])
+            embed = discord.Embed(title=taco[1])
+            embed.set_author(
+                name=guild_member.display_name,
+                icon_url=guild_member.avatar_url
             )
             await ctx.channel.send(embed=embed)
+
+    else:
+        embed = discord.Embed(
+            title="No tacos awarded this month",
+            description="Go get some Tacos!"
+        )
+        await ctx.channel.send(embed=embed)
+
+
+if __name__ == "__main__":
+    load_dotenv()
 
     client.run(os.getenv("DISCORD_KEY"))
